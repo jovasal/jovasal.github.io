7e6aff05c621ada571100c232e070098e16039bb
jovasal <jovasal@gmail.com>
2023-06-24 16:07:45 -0500

    no solution yet
    no yet

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,14 @@
 function MobileMenu() {
     const menuHamburguer = document.querySelector('.burguer');
+    const navigationMobile = document.querySelector('.menu-nav');
+    const navMobileLinks = document.querySelectorAll('.links');
+    const hamburgerImage = document.querySelector('#burguer');
+
+    function toggleMenu() {
+        if (navMobile.classList.contains('active')) {
+          hamburgerImage.src = './images/cancel.svg';
+        } else {
+          hamburgerImage.src = './images/Icon - Menu.svg';
+        }
+      }
 }
