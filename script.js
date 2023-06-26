2574b02536320bb198bd4cb895c159a0f17df387
jovasal <jovasal@gmail.com>
2023-06-26 12:50:28 -0500

    script modified
    script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const menu = document.getElementById('menu-nav');
+const burgerButton = menu.querySelector('.burger');
+/*const links = menu.querySelector('.links');*/
+
+
+function toggleMenu() {
+  const menu = document.getElementById('menuinit');
+  const links = menu.querySelector('.links');
+  links.classList.toggle('show');
+}
