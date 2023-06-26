3e00e17d0591d8bbc6cac8fbc3f26c443372c060
jovasal <jovasal@gmail.com>
2023-06-26 15:00:13 -0500

    change in menu
    changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const menu = document.getElementById('menu-nav');
+/*const menu = document.getElementById('menu-nav');
 const burgerButton = menu.querySelector('.burger');
 const links = menu.querySelector('.links');
 
@@ -7,4 +7,22 @@ function toggleMenu() {
   const menu = document.getElementById('menu-nav');
   const links = menu.querySelector('.links');
   links.classList.toggle('show');
-}
+}*/
+
+
+
+//get necesary element
+
+var menuIcon = document.getElementById("menuIcon");
+var closeIcon = document.getElementById("closeIcon");
+var links = document.getElementById("links");
+
+
+//Add even listener to the menu icon
+
+menuIcon.addEventListener("click", function() {
+  menuIcon.style.display = "none";
+  closeIcon.style.display = "inline-block";
+  links.classList.add("open");
+});
+
