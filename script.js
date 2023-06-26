d780c6ba156bb980b235ec223d7c50fef82c1715
jovasal <jovasal@gmail.com>
2023-06-26 10:49:45 -0500

    return backup
    return at the bewginning

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 const.menuhamburguer = document.querySelector(.burger);
 const.Navhead = document.querySelector(.head1);
 const.Navlinks = document.querySelector(.links);
-const.Navlinks = document.querySelector(#burger);
+const.burgerimage = document.querySelector(#burger);
+
+/*
+function toggleMenu() {
+    if (navhead.classList.contains('active')) {
+      hamburgerImage.src = './images/cancel.svg';
+    } else {
+      hamburgerImage.src = './images/burger.png';
+    }
+  }
+ 
+  menuhamburguer.addEventListener('click', () => {
+    Navhead.classList.toggle('active');
+    toggleMenu();
+  });
+  
+  Navlinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      navMobile.classList.remove('active');
+      toggleMenu();
+    });
+  });*/
