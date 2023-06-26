9643d5c5be0fba6b78111a0939854635df5273b6
jovasal <jovasal@gmail.com>
2023-06-26 14:09:48 -0500

    menu
    menu

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const menu = document.getElementById('menu-nav');
 const burgerButton = menu.querySelector('.burger');
-/*const links = menu.querySelector('.links');*/
+const links = menu.querySelector('.links');
 
 
 function toggleMenu() {
-  const menu = document.getElementById('menuinit');
+  const menu = document.getElementById('menu-nav');
   const links = menu.querySelector('.links');
   links.classList.toggle('show');
 }
