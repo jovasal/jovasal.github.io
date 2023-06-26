64ae44d329b90214cdc2def929621fccb2721b49
jovasal <jovasal@gmail.com>
2023-06-26 09:40:37 -0500

    changes required
    change??

---
+++ script.js
@@ -0,0 +1,4 @@
+const.menuhamburguer = document.querySelector(.burger);
+const.Navhead = document.querySelector(.head1);
+const.Navlinks = document.querySelector(.links);
+const.Navlinks = document.querySelector(#burger);
