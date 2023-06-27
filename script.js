7a4ffce961d47d45e5534a941c0a205bdc76ed2d
jovasal <jovasal@gmail.com>
2023-06-27 17:47:37 -0500

    clone a menu
    clone

---
+++ script.js
@@ -0,0 +1,24 @@
+const menuHamburger = document.querySelector('.hamburguer');
+const navMobile = document.querySelector('.navbar');
+const navMobileLinks = document.querySelectorAll('.nav-list a');
+const hamburgerImage = document.querySelector('#hamburguer');
+
+function toggleMenu() {
+  if (navMobile.classList.contains('active')) {
+    hamburgerImage.src = './images/cancel.svg';
+  } else {
+    hamburgerImage.src = './images/Icon - Menu.svg';
+  }
+}
+
+menuHamburger.addEventListener('click', () => {
+  navMobile.classList.toggle('active');
+  toggleMenu();
+});
+
+navMobileLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navMobile.classList.remove('active');
+    toggleMenu();
+  });
+});
