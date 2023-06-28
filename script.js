1f161d2d7533529e88dc5e17c33f3bddfe95df3e
jovasal <jovasal@gmail.com>
2023-06-27 21:22:50 -0500

    first card
    first card

--- script.js
+++ script.js
@@ -22,3 +22,41 @@ navMobileLinks.forEach((link) => {
     toggleMenu();
   });
 });
+
+
+
+
+
+
+// Get the necessary DOM elements
+const popupButton = document.getElementById('popupButton');
+const popupContainer = document.getElementById('popupContainer');
+const popup = document.getElementById('popup');
+const popupTitle = document.querySelector('.popup-title');
+const closeButton = document.querySelector('.popup-close');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+// Add event listeners
+popupButton.addEventListener('click', function() {
+  // Set the dynamic content
+  popupTitle.textContent = 'Tonic';
+
+  // Show the popup
+  popupContainer.style.display = 'block';
+});
+
+closeButton.addEventListener('click', function() {
+  // Hide the popup
+  popupContainer.style.display = 'none';
+});
+
+button1.addEventListener('click', function() {
+  // Button 1 functionality
+  console.log('Button 1 clicked');
+});
+
+button2.addEventListener('click', function() {
+  // Button 2 functionality
+  console.log('Button 2 clicked');
+});
