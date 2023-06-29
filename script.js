28ae5664a803329275f61a27be9d3f20dcd7b668
jovasal <jovasal@gmail.com>
2023-06-29 18:36:30 -0500

    saving dates in formData
    done

--- script.js
+++ script.js
@@ -127,58 +127,22 @@ email.addEventListener('input', (event) => {
   }
 });
 
+// Check if there is any data in local storage
+if (localStorage.getItem('formData')) {
+  const savedData = JSON.parse(localStorage.getItem('formData'));
+  document.getElementById('name').value = savedData.name || '';
+  document.getElementById('email').value = savedData.email || '';
+  document.getElementById('message').value = savedData.message || '';
+}
 
-// Function to save input data to local storage
-
-export default function preserveData() {
-  function saveFormData() {
-    const nameInput = document.getElementById('name');
-    const emailInput = document.getElementById('email');
-    const messageInput = document.getElementById('message');
-
-    // Create an object to store the form data
+// Add event listeners to input fields
+const inputFields = document.querySelectorAll('input');
+inputFields.forEach(input => {
+  input.addEventListener('change' () => {
     const formData = {
-      name: nameInput.value,
-      email: emailInput.value,
-      message: messageInput.value,
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      message: document.getElementById('message').value,
     };
-
-    // Convert the object to JSON string
-    const formDataJSON = JSON.stringify(formData);
-
-    // Save the JSON string to local storage
-    localStorage.setItem('formData', formDataJSON);
-  }
-
-  // Function to load and pre-fill data from local storage
-  function loadFormData() {
-    const nameInput = document.getElementById('name');
-    const emailInput = document.getElementById('email');
-    const messageInput = document.getElementById('message');
-
-    // Get the saved JSON string from local storage
-    const formDataJSON = localStorage.getItem('formData');
-
-    // Parse the JSON string into an object
-    const formData = JSON.parse(formDataJSON);
-
-    if (formData) {
-      // Pre-fill the input fields with the saved values
-      nameInput.value = formData.name || '';
-      emailInput.value = formData.email || '';
-      messageInput.value = formData.message || '';
-    }
-  }
-
-  // Add event listeners to the input fields
-  const nameInput = document.getElementById('name');
-  nameInput.addEventListener('input', saveFormData);
-
-  const emailInput = document.getElementById('email');
-  emailInput.addEventListener('input', saveFormData);
-
-  const messageInput = document.getElementById('message');
-  messageInput.addEventListener('input', saveFormData);
-
-  // Load the form data when the page is loaded
-  window.addEventListener('load', loadFormData);
+  })
+})
